180c2b50621bf5ee592b772898a1ad64aed42305
franloudent-francis <jj1513@srmist.edu.in>
2025-04-17 13:07:39 +0530

    ci:setup medusa sdk

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,7 @@
 'use client'
+import { sdk } from '@/lib/medusa/config'
 import { useQuery } from '@tanstack/react-query'
+import { HttpTypes } from "@medusajs/types"
 
 export const fetchUsers = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users')
@@ -7,20 +9,36 @@ export const fetchUsers = async () => {
   return res.json()
 }
 
+export const fetchRegions = async (): Promise<HttpTypes.StoreRegion[]> => {
+    const res = await sdk.store.region.list()
+    return res.regions
+  }
+
 export default function UserList() {
   const { data, isLoading, error } = useQuery({
     queryKey: ['users'],
     queryFn: fetchUsers,
   })
 
+  const {data:regionData,isLoading:isLoadingRegion,error:errorRegion} = useQuery<HttpTypes.StoreRegion[]>({
+    queryKey: ['regions'],
+    queryFn: fetchRegions,
+  })
+
+  console.log('regionData', regionData)
   if (isLoading) return <p>Loading...</p>
   if (error instanceof Error) return <p>Error: {error.message}</p>
 
   return (
     <ul>
-      {data.map((user: any) => (
+      {/* {data.map((user: any) => (
         <li key={user.id}>{user.name}</li>
-      ))}
+      ))} */}
+      {
+        regionData?.map((region) => (
+          <li key={region.currency_code}>{region.name}</li>
+        ))
+      }
     </ul>
   )
 }
